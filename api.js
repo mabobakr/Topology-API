317f222682990bbd4791e58bbc7832b9846b4b79
mabobakr <m.abobakr10@yahoo.com>
2021-09-28 19:41:53 +0200

    Add documetation Add Error logging Move queryDevicesWithNetlistNode implementation inside topology class

--- api.js
+++ api.js
@@ -2,47 +2,120 @@ const Topology = require("./topology");
 
 let topologies = [];
 
+/**
+ * @function readJSON
+ * @param {String} fileName
+ * @description Read a topology from given json file  
+ * @returns {Boolean} True if the operation was successful and false otherwise
+ */
 exports.readJSON = function (fileName) {
   try {
+	// Create new topology and add it to topologies array
     topologies.push(new Topology(fileName));
+    return true;
   } catch (err) {
-    console.log(err.message);
-    // return result
+	// In case of error, log the error and return false
+    console.error(err);
+    return false;
   }
 };
 
+/**
+ * @function writeJSON
+ * @param {String} topologyId
+ * @description Write topology specified by topologyId to json file  
+ * @returns {Boolean} True if the operation was successful and false otherwise
+ */
+exports.writeJSON = function (topologyId) {
+  try {
+	// Find required topology
+    let topology = topologies.find((topology) => topology.id == topologyId);
+    
+	// Throw error if topology doesn't exist
+	if (!topology) throw new Error("The given topology doesn't exist");
+    
+	// Write topology to json file
+	topology.writeToFile();
+    return true;
+  } catch (err) {
+	// In case of error, log the error and return false
+	console.error(err);
+    return false;
+  }
+};
+
+/**
+ * @function queryTopologies
+ * @description Get topologies are currently in memory  
+ * @returns {Topology[]} Array of topologies that are currently in memory
+ */
 exports.queryTopologies = function () {
   return topologies;
 };
 
-exports.writeJSON = function (topologyId) {
-  let topology = topologies.find((topology) => topology.id == topologyId);
-  if (topology) topology.writeToFile();
-  // return result
+/**
+ * @function deleteTopology
+ * @param {String} topologyId
+ * @description Delete topology specified by topologyId   
+ * @returns {Boolean} True if the operation was successful and false otherwise
+ */
+ exports.deleteTopology = function (topologyId) {
+  try {
+	// Find index of topology
+    let index = topologies.findIndex((topology) => topology.id == topologyId);
+    
+	// Throw error if topology doesn't exist
+	if (index == -1) throw new Error("The given topology doesn't exist");
+    
+	// Remove topology from memory
+	topologies.splice(index, 1);
+    return true;
+  } catch (err) {
+	// In case of error, log the error and return false
+    console.error(err);
+    return false;
+  }
 };
 
-exports.deleteTopology = function (topologyId) {
-  topologies.splice(
-    topologies.findIndex((topology) => topology.id == topologyId),
-    1
-  );
-  // return result
-};
-
+/**
+ * @function queryDevices
+ * @param {String} topologyId
+ * @description Get devices of the topology specified by topologyId   
+ * @returns {Object[]} Array of objects representing devices in the topology
+ * @returns {null} If topology doesn't exist
+ */
 exports.queryDevices = function (topologyId) {
+  // Find required topology
   let topology = topologies.find((topology) => topology.id == topologyId);
+  
+  // If found return its components 
   if (topology) return topology.components;
+  
+  // If not found log the error and return null
+  console.error("The given topology doesn't exist");
+  return null;
 };
+
+/**
+ * @function queryDevicesWithNetlistNode
+ * @param {String} topologyId
+ * @param {String} NetlistNodeId
+ * @description Get devices of the topology specified by topologyId that are connected to given netlist node  
+ * @returns {Object[]} Array of objects representing required devices in the topology
+ * @returns {null} If topology doesn't exist
+ */
 
 exports.queryDevicesWithNetlistNode = function (topologyId, NetlistNodeId) {
-  let components = this.queryDevices(topologyId);
+  // Find required topology
+  let topology = topologies.find((topology) => topology.id == topologyId);
+  
+  // If not found, log the error and return null
+  if (!topology) {
+    console.error("The given topology doesn't exist");
+    return null;
+  }
 
-  var nodes;
-  return components.filter((component) => {
-    nodes = Object.values(component.netlist);
-    for (let i = 0; i < nodes.length; i++) {
-      if (nodes[i] == NetlistNodeId) return true;
-    }
-    return false;
-  });
+  // If found, return array of devices
+  let devices = topology.getNodeDevices(NetlistNodeId);
+  return devices;
 };
--- topology.js
+++ topology.js
@@ -1,17 +1,53 @@
 const fs = require("fs");
 
+/**
+ * Topology Class
+ */
 class Topology {
+  /**
+   * @constructor
+   * @param {String} fileName
+   */
   constructor(fileName) {
     let file = fs.readFileSync(fileName);
+
+    // Convert json string to Object
     file = JSON.parse(file);
+
     this.id = file.id;
     this.components = file.components;
   }
 
+  /**
+   * @function writeToFile
+   * @description Write this topology to a file
+   */
   writeToFile() {
-    fs.writeFileSync("topology_" + this.id +".json", JSON.stringify(this, null, 2));
+    fs.writeFileSync(
+      "topology_" + this.id + ".json",
+      JSON.stringify(this, null, 2)
+    );
+  }
+
+  /**
+   * @function getNodeDevices
+   * @param {String} NetlistNodeId
+   * @description  Get devices of this topology that are connected to the given netlist node
+   * @returns {Object[]} array of devices of this topology that are connected to the given netlist node
+   */
+  getNodeDevices(NetlistNodeId) {
+	var nodes;
+    // Only return components that are connected to NetlistNode
+    return this.components.filter((component) => {
+      // Get values of netlist object
+      nodes = Object.values(component.netlist);
+
+      for (let i = 0; i < nodes.length; i++) {
+        if (nodes[i] == NetlistNodeId) return true;
+      }
+      return false;
+    });
   }
 }
 
-
-module.exports = Topology
+module.exports = Topology;
